4f5b91d13ecc28e7c7a75d6937522b72d1ad49da
Герман <german@MacBook-Air-German.local>
2021-03-08 02:32:56 +0300

    succession reversed

--- mydb.sql
+++ mydb.sql
@@ -1,15 +1,15 @@
-create table if not exists Tracks(
-	Title varchar(40) primary key, 
-	Duration timestamp not null
-);
-
 create table if not exists Albums(
 	AlbumTitle varchar(40) primary key unique, 
 	Release_Year integer not null check(Release_Year>0),
-	TracksTitles varchar(40) unique references Tracks(Title)
+	Tracks varchar(40) unique
+);
+
+create table if not exists Tracks( 
+	Duration timestamp not null,
+	TrackTitle varchar(40) primary key references Albums(Tracks)	
 );
 
 create table if not exists Musicians(
 	Name varchar(40) primary key, 
-	AlbumsTracks varchar(40) references Albums(TracksTitles)
+	AlbumsTitles varchar(40) references Albums(AlbumTitle)
 );
