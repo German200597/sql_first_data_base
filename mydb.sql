af7fbf4cf170fee5ec7e7e3bda4ff8aab1f7d2cb
Герман <german@MacBook-Air-German.local>
2021-03-09 16:45:14 +0300

    new changes

--- mydb.sql
+++ mydb.sql
@@ -5,13 +5,15 @@ create table if not exists Musicians(
 
 
 create table if not exists Albums(
-	id_album serial not null,
-	AlbumTitle varchar(40) primary key unique, 
+	id_album serial primary key,
+	AlbumTitle varchar(40) not null, 
 	Release_Year integer not null check(Release_Year>0),
 	Singer varchar(40) unique references Musicians(id_musician)
 );
 
 create table if not exists Tracks( 
+	id_track serial primary key,
+	TrackTitle varchar(40) not null,
 	Duration timestamp not null,
-	TrackTitle varchar(40) primary key references Albums(Tracks)	
+	Album varchar(40) primary key references Albums(id_album)	
 );
